e2aa96589a94d9a45957414a8fc284a0dd3ff420
Alesia - Imali SuMaku Kute <73201769+AlesiaLRuffin@users.noreply.github.com>
2020-11-04 20:46:46 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,27 +23,44 @@ namespace VacationBudgetPlanner
             Console.WriteLine("Welcome Adventurer " + adventurerName + ", where do you plan to go?");
             Console.WriteLine("1: Mountain biking in Blue Derby, Tasmania");
             Console.WriteLine("2: Ballroom Dancing on a cruise to Italy, Greek Isles and more?");
+            DoOver:
             Console.WriteLine("Enter 1 or 2");
             string userValue = Console.ReadLine();
 
+            // week 1 user destination choices
             if (userValue == "1")
             {
-                string message = "WOW you're a Mountain Mangler! You'll be shredding on mountain bike trails in Blue Derby, Tasmania";
+                string message = ($"WOW {adventurerName} you're a Mountain Mangler! You'll be shredding on mountain bike trails in Blue Derby, Tasmania");
                 Console.WriteLine(message);
+
+                Console.WriteLine($"How many days to do plan to be in Tasmania?");
+                int numberOfDays = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"How much spending money are you planning to take with you?");
+                double spendingMoney = Convert.ToDouble(Console.ReadLine());
+                double perDiem = Math.Round(spendingMoney / numberOfDays, 2);
+
+                Console.WriteLine($"Adventurer {adventurerName}, you are going mountain biking in Blue Derby, Tasmania" +
+                    $" for {numberOfDays} days with {spendingMoney.ToString("C2", CultureInfo.CurrentCulture)} in spending money.");
+                Console.WriteLine($"This allows for {perDiem.ToString("C2", CultureInfo.CurrentCulture)} to spend per day.");
+                Console.WriteLine("Press enter to exit");
+                //Console.ReadLine();
             }
             else if (userValue == "2")
             {
-                string message = "Hey Twinkletoes, you will be waltzing under the stars on a cruise to Italy, " +
-                    "the Greek Isles and other exciting destinations.";
+                string message = ($"Hey Twinkletoes {adventurerName}, you will be waltzing under the stars on a cruise to Italy, " +
+                    "the Greek Isles and other exciting destinations.");
                 Console.WriteLine(message);
            }
             else
             {
                 string message = "Sorry, that option isn't available. Please choose 1 or 2";
                 Console.WriteLine(message);
+                goto DoOver;
             }
             Console.ReadLine();
 
+
+            
 
         }
     }
